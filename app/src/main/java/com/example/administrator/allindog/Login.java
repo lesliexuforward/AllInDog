dd6d620a1eea36a464f79c4b1eabf4cc053a2c3c
xujiang <978985466@qq.com>
2016-11-07 23:18:20 +0800

    11.7寝室
    11.7

---
+++ app/src/main/java/com/example/administrator/allindog/Login.java
@@ -0,0 +1,29 @@
+package com.example.administrator.allindog;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class Login extends AppCompatActivity {
+    private Button registerButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        registerButton=(Button)findViewById(R.id.create);
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setClass(Login.this,Register.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/administrator/allindog/Register.java
@@ -0,0 +1,16 @@
+package com.example.administrator.allindog;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/11/7.
+ */
+public class Register extends AppCompatActivity{
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+    }
+}
